bb0a77c2cd562c0894af8f1ca8c149b69fd58e65
mohammad-mohamed <mohammad.mhm16@gmail.com>
2024-07-01 16:26:31 +0200

    navbar and footer made, home page component made

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
+  
 </template>
 
-<style>
+<style src="@/assets/css/style.css">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="container-fluid">
+        <div class="row">
+            <p class="lead">Mohammad Mohamed &copy; <span class="shadow">{{ aYear }}</span></p>
+        </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: "FooterComp",
+    data() {
+        return {
+            aYear: new Date().getUTCFullYear()
+        }
+    }
+}
+</script>
+<style scoped>
+    
+</style>
