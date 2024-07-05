442ddbf50194f9573ad203875032affd9833c453
mohammad-mohamed <mohammad.mhm16@gmail.com>
2024-07-05 16:15:06 +0200

    moving styling

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -78,5 +78,121 @@
   text-decoration: none;
   color: inherit;
 }
+
+/* about me */
+.about-me-title {
+  background: linear-gradient(180deg, #9360ff 0%, #8f1dd5 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-weight: 800;
+  font-size: 35px;
+  line-height: 102%;
+  font-family: Poppins, sans-serif;
+}
+
+.about-me-description {
+  color: #c5c5c5;
+  letter-spacing: 0.36px;
+  font-weight: 300;
+  font-size: 18px;
+  font-family: Poppins, sans-serif;
+  text-align: justify;
+}
+
+.about-me-details-label p,
+.about-me-details-value p {
+  color: #161513;
+  margin: 0;
+}
+
+.about-me-details-label {
+  text-align: left;
+}
+
+.about-me-details-value {
+  text-align: left;
+}
+
+.about-me-actions {
+  font-size: 21px;
+  font-weight: 600;
+}
+
+.about-me-contact-btn,
+.about-me-download-btn {
+  font-family: Poppins, sans-serif;
+  border-radius: 50px;
+  padding: 24px 30px;
+  text-align: center;
+  display: inline-block;
+  background: linear-gradient(0deg, #fff 0%, #fff 100%);
+  color: #161513;
+  border: 1px solid #fff;
+  margin-bottom: 20px;
+  transition: transform 0.3s ease-in-out, color 0.3s ease-in-out;
+}
+
+.about-me-contact-btn:hover,
+.about-me-download-btn:hover {
+  animation: growShrink 0.5s infinite alternate;
+  color: #161513; 
+}
+
+@keyframes growShrink {
+  0% {
+    transform: scale(1);
+    color: #161513; 
+
+  }
+  100% {
+    transform: scale(1.1);
+    color: #161513; 
+
+  }
+}
+
+/* #about {
+  margin-bottom: 120px;
+} */
+
+#textss p {
+  color: #c5c5c5;
+}
+
+@media (max-width: 768px) {
+  .about-me-actions {
+    text-align: center;
+  }
+}
+@media (max-width: 700px) {
+  .about-me-title {
+    font-size: 28px;
+  }
+  .about-me-description {
+    font-size: 16px;
+  }
+  .about-me-contact-btn,
+  .about-me-download-btn {
+    padding: 20px 25px;
+    font-size: 18px;
+  }
+}
+
+@media (max-width: 300px) {
+  .about-me-title {
+    font-size: 24px;
+  }
+  .about-me-description {
+    font-size: 14px;
+  }
+  .about-me-contact-btn,
+  .about-me-download-btn {
+    padding: 18px 22px;
+    font-size: 16px;
+  }
+}
+
+  
 </style>
   
--- src/components/ResumeSection.vue
+++ src/components/ResumeSection.vue
@@ -85,4 +85,116 @@
   height: 15vh;
 }
 
+/* education */
+
+.education-section {
+    display: flex;
+    flex-direction: column;
+    max-width: 799px;
+    padding: 0 20px;
+    margin: 0 auto; /* Center horizontally */
+  }
+  
+  .education-title {
+    margin-top: 100px;
+    text-transform: uppercase;
+    background: linear-gradient(180deg, #ff5b5b 0%, #d11313 100%);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    align-self: center;
+    font: 700 25px/102% Poppins, sans-serif;
+  }
+  
+  .education-item {
+    display: flex;
+    width: 100%;
+    gap: 20px;
+    font-weight: 400;
+    margin-top: 88px;
+  }
+  
+  @media (max-width: 991px) {
+    .education-item {
+      flex-wrap: wrap;
+      margin-top: 40px;
+    }
+  }
+  
+  .education-degree {
+    color: var(--dark-content, #666);
+    letter-spacing: 1px;
+    flex-grow: 1;
+    flex-basis: auto;
+    font: 20px/140% Poppins, sans-serif;
+  }
+  
+  @media (max-width: 991px) {
+    .education-degree {
+      max-width: 100%;
+    }
+  }
+  
+  .education-details {
+    align-self: start;
+    display: flex;
+    gap: 7px;
+    font-size: 14px;
+    color: var(--Light-Grey, #8491a0);
+    text-align: center;
+    line-height: 150%;
+  }
+  
+  .education-icon {
+    width: 18px;
+    aspect-ratio: 1.39;
+    object-fit: auto;
+    object-position: center;
+  }
+  
+  .education-date {
+    font-family: Poppins, sans-serif;
+    flex-grow: 1;
+    flex-basis: auto;
+  }
+  
+  .education-institute {
+    align-self: start;
+    display: flex;
+    gap: 5px;
+    font-size: 12px;
+    color: var(--light-Content, #a7a7a7);
+    font-weight: 500;
+    letter-spacing: 1px;
+    line-height: 233%;
+  }
+  
+  .education-location-icon {
+    width: 18px;
+    aspect-ratio: 1.49;
+    object-fit: auto;
+    object-position: center;
+    align-self: start;
+  }
+  
+  .education-location {
+    font-family: Poppins, sans-serif;
+    flex-grow: 1;
+    flex-basis: auto;
+  }
+  
+  .education-separator {
+    width: 100%;
+    min-height: 2px;
+    margin-top: 25px;
+    background-color: var(--Colors-Grey, #ebeaed);
+  }
+  
+  @media (max-width: 991px) {
+    .education-separator {
+      max-width: 100%;
+    }
+  }
+  
+
 </style>
